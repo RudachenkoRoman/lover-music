0e8ca56016f937fc72648d71951308301f52242e
Rudachenko Roman <rudachenko93@mail.ru>
2022-03-14 22:49:11 +0500

    Доработка по замечаниям

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,31 +1,44 @@
 package ru.netology
 
+import kotlin.math.ceil
+
 fun main() {
-    val itemPrice = 1
+    val itemPrice = 100
     val discountOneHundred = 100
     val discountOneLevel = 1_001
     val discountTwoLevel = 10_001
 
     print("Введите количество музыкальных произведений для покупки: ")
     val itemCount = readLine()?.toInt() ?: return
-    val totalPrice = itemPrice * itemCount
-    println("Покупка - $totalPrice рублей →\n")
-    var result = if (totalPrice < discountTwoLevel){ ((totalPrice - discountOneHundred).toDouble()) }
-    else { (totalPrice - (totalPrice / 100 * 5)).toDouble() }
+    var result: Double
+    val totalPrice: Int
+    if (itemCount <= 0) {
+        println("Вы ввели неверную сумму. Сумма должна быть больше 0")
+        return
+    } else {
+        totalPrice = itemPrice * itemCount
+        println("Покупка - $totalPrice рублей →\n")
+        result = when (totalPrice) {
+            in discountOneLevel..discountTwoLevel -> (totalPrice - discountOneHundred).toDouble()
+            in discountTwoLevel..Int.MAX_VALUE -> (totalPrice - (totalPrice / 100 * 5)).toDouble()
+            else -> {
+                totalPrice.toDouble()
+            }
+        }
+    }
 
-when (totalPrice) {
-    in discountOneLevel..10_000 -> println("После применения скидки 100 рублей - $result рублей.\n")
-    in discountTwoLevel..Int.MAX_VALUE -> println("После применения скидки 5% - $result рублей.\n")
-}
+    when (totalPrice) {
+        in discountOneLevel..10_000 -> println("После применения скидки 100 рублей - ${result.toInt()} рублей.\n")
+        in discountTwoLevel..Int.MAX_VALUE -> println("После применения скидки 5% - ${result.toInt()} рублей.\n")
+    }
 
-val randomValue = (1..2).random()
-if (randomValue == 1) {
-    println("Вы настоящий \"меломан\"!, поэтому мы даем вам дополнительную скидку 1%!! ")
-    result *= 0.99
-    val integer = result.toInt()
-    val double = (result - integer) * 100
-    println("После применения 1% скидки - $integer рублей ${double.toInt()} копеек.")
-}
+    val randomValue = (1..2).random()
+    if (randomValue == 1) {
+        println("Вы настоящий \"меломан\"!, поэтому мы даем вам дополнительную скидку 1%!! ")
+        result *= 0.99
+        val pennies = (result - result.toInt()) * 100
+        println("После применения 1% скидки - ${result.toInt()} рублей ${ceil(pennies).toInt()} копеек.")
+    }
 }
 
 
