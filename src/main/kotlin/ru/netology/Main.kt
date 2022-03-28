7824c99f00230050cc3d087914089f442eb371f1
Rudachenko Roman <rudachenko93@mail.ru>
2022-03-28 23:43:37 +0500

    Доработка по замечаниям

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -10,34 +10,28 @@ fun main() {
 
     print("Введите количество музыкальных произведений для покупки: ")
     val itemCount = readLine()?.toInt() ?: return
-    var result: Double
-    val totalPrice: Int
     if (itemCount <= 0) {
         println("Вы ввели неверное количество. Количество должно быть больше 0")
         return
-    } else {
-        totalPrice = itemPrice * itemCount
-        println("Покупка - $totalPrice рублей →\n")
-        result = when (totalPrice) {
-            in discountOneLevel..discountTwoLevel -> (totalPrice - discountOneHundred).toDouble()
-            in discountTwoLevel..Int.MAX_VALUE -> (totalPrice - (totalPrice / 100 * 5)).toDouble()
-            else -> {
-                totalPrice.toDouble()
-            }
-        }
     }
-
-    when (totalPrice) {
-        in discountOneLevel..10_000 -> println("После применения скидки 100 рублей - ${result.toInt()} рублей.\n")
-        in discountTwoLevel..Int.MAX_VALUE -> println("После применения скидки 5% - ${result.toInt()} рублей.\n")
+    var totalPrice: Double = (itemPrice * itemCount).toDouble()
+    println("Покупка - ${totalPrice.toInt()} рублей →\n")
+    if (totalPrice > discountOneLevel) {
+        if (totalPrice > discountTwoLevel) {
+            totalPrice *= 0.95
+            println("После применения 5% скидки - ${totalPrice.toInt()} рублей\n")
+        } else {
+            totalPrice -= discountOneHundred
+            println("После применения скидки 100 рублей - ${totalPrice.toInt()} рублей\n")
+        }
     }
 
     val randomValue = (1..2).random()
     if (randomValue == 1) {
         println("Вы настоящий \"меломан\"!, поэтому мы даем вам дополнительную скидку 1%!! ")
-        result *= 0.99
-        val pennies = (result - result.toInt()) * 100
-        println("После применения 1% скидки - ${result.toInt()} рублей ${ceil(pennies).toInt()} копеек.")
+        totalPrice *= 0.99
+        val pennies = (totalPrice - totalPrice.toInt()) * 100
+        println("После применения 1% скидки - ${totalPrice.toInt()} рублей ${ceil(pennies).toInt()} копеек.")
     }
 }
 
